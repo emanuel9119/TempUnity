24c7ab7215794d7914b54cfd90bb8fa4ac5ac8b6
LAPTOP-OGOLEBG2\Lachlan Hogno <lhogno@gmail.com>
2016-08-16 20:06:32 +1000

    Added some stripts and prefabs Player Script currently incomplete, unsure how to finish

---
+++ UnityTemp/Assets/Scene 2/Scripts/BulletScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 1.5f);
+	}
--- UnityTemp/Assets/Scene 2/Scripts/PlayerCont.cs
+++ UnityTemp/Assets/Scene 2/Scripts/PlayerCont.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class PlayerCont : MonoBehaviour {
+public Rigidbody BulletPrefab
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		if Input.GetMouseButtonDown(0)) {
+			Rigidbody BulletInstance;
+			BulletInstance = Instantiate(BulletPrefab, PlayerFront.position, ) as Rigidbody
+				BulletInstance.AddForce(5000)
+		}
 	}
 }
